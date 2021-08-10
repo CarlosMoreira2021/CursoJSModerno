7042e668a2f3db89b9230695791bcf67ea45bac5
Carlos Eduardo Moreira <carlos202097vr@hotmail.com>
2021-08-10 11:52:45 -0300

    comit 10/08/21

---
+++ aula12/ex013.js
@@ -0,0 +1,41 @@
+var agora = new Date();
+var diaSem = agora.getDay();
+
+/*
+0 DOMINGO
+1 SEGUNDA
+2 TERCA
+3 QUARTA
+4 QUINTA 
+5 SEXTA
+6 SABADO
+*/
+
+
+switch(diaSem){
+    case 0 :
+        console.log('domingo')
+    break;
+    case 1 :
+        console.log('segunda')
+    break;
+    case 2:
+        console.log('terca')
+    break;
+    case 3 :
+        console.log('quarta')
+    break;
+    case 4 :
+        console.log('quinta')
+    break;
+    case 5 :
+        console.log('sexta')
+    break;
+    case 6 :
+        console.log('sabado')
+    break;
+    default:
+        console.log('dia invalido')
+    break
+
+}
