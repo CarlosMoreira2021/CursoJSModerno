7e08e22d9fae41e60fa4709d98fa8e9d82c8260c
Carlos Eduardo Moreira <carlos202097vr@hotmail.com>
2021-08-09 21:59:22 -0300

    commit 09/08/2021

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 60.5;
+
+console.log(`A velocidade do seu carro é ${vel}`);
+
+if (vel > 60) { // condicao simples
+  console.log(
+    "voce ultrapassou a velocidade permitida e por isso foi MULTADO!"
+  );
+} 
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Eua'
+
+if(pais == 'Brasil'){
+    console.log(' voce é brasileiro')
+}else{
+    console.log (' voce é estrangeiro')
+}
