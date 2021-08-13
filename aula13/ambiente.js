48832c89104a4560d1b90ff3e9d95ff01a7a4fd7
Carlos Eduardo Moreira <carlos202097vr@hotmail.com>
2021-08-12 23:41:40 -0300

    commit 12/08/21

---
+++ aula13/ambiente.js
@@ -0,0 +1,37 @@
+var c = 1
+//enquanto "while" a condicao forr verdadeira  faca"faca" a repeticao
+do{
+    console.log(`Passo ${c}`)
+    c++
+}while (c <= 5)
+//estrutura de repeticao com teste logico no final
+
+//do = faca
+
+
+
+
+/*var c = 1
+while(c <= 5){
+    console.log(`Passo ${c}`)
+    c++
+}*/
+//estrutura de repeticao com teste logo no inicio
+
+
+
+
+
+
+
+
+
+/*
+console.log('tudo bem ?')
+console.log('tudo bem ?')
+console.log('tudo bem ?')
+console.log('tudo bem ?')
+console.log('tudo bem ?')
+console.log('tudo bem ?')
+console.log('tudo bem ?')
+*/
